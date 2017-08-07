e495c9a13aacd1c6fd5e50d2d3f938c97ca3d40b
Lokesh Karanam <sunnylokesh3@gmail.com>
2017-08-07 10:49:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'sunnylokesh3@gmail.com';                 // SMTP username
-$mail->Password = '9april1997';                           // SMTP password
+$mail->Password = '********';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
